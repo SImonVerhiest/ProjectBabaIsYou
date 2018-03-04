2ef48af32bd40e63de990f436276c3bb58ffc22f
Simon Verhiest <simonverhiest@gmail.com>
2018-03-04 12:58:00 +0100

    Classe pour les mouvements.
    Ébauche de la classe qui géra les mouvements de baba,...

---
+++ Moves.java
@@ -0,0 +1,35 @@
+public class Moves{
+
+	public int getPosCol(String[][] tab){
+		int pos = 0;
+
+		return pos;
+	}
+
+	public int getPosRow(String [][] tab){
+		int pos = 0;
+
+		return pos;
+	}
+
+	public void goUp(String [][] tab){
+		int i = getPosRow(tab);
+		i = i-1;
+	}
+
+	public void goDown(String [][] tab){
+		int i = getPosRow(tab);
+		i = i+;
+	}
+
+	public void goLeft(String [][] tab){
+		int i = getPosCol(tab);
+		i = i-1;
+	}
+
+	public void goRight(String [][] tab){
+		int i = getPosCol(tab);
+		i = i+1;
+	}
+	
+}
