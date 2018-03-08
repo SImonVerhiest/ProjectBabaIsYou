edc996d27f2907189aad9cae33ba9baf4bb4a5e3
Simon Verhiest <simonverhiest@gmail.com>
2018-03-08 13:50:26 +0100

    Update Board.java

--- Board.java
+++ Board.java
@@ -2,35 +2,52 @@ import java.io.*;
 
 public class Board{
 
-	public static void main(String[] args) throws IOException{
-		// this will read the specified file
-		String level = args[0];
-		FileReader fr = new FileReader("D:\\Documents\\Cours 2017-2018\\Projet\\"+level+".txt");
-		BufferedReader br = new BufferedReader(fr);
-		String line;
-		line = br.readLine();
-		String columnLength = line.substring(0,2);
-		String rowLength = line.substring(3,5);
+	public static void main(String[] args){
+		String niveau = args[0];	
+		Board jeu = new Board(niveau);	
 		
-		int column = Integer.parseInt(columnLength) + 1;
-		int row = Integer.parseInt(rowLength) + 1;
-		String [][]board = new String[column][row];
 
-		while((line= br.readLine()) != null){
-			String pos =line.substring(line.indexOf(" "));
-			String temp_i = pos.substring(pos.indexOf(" "), pos.lastIndexOf(" "));
-			String temp_j = pos.substring(pos.lastIndexOf(" "));
-			temp_i = temp_i.replaceAll("\\s","");
-			temp_j = temp_j.replaceAll("\\s","");
-			int i = Integer.parseInt(temp_i);  // col
-			int j = Integer.parseInt(temp_j); // row
-			board[i][j] = line.substring(0,line.indexOf(" "));
-			
+	}
+	//Constructor
+	public Board(String level){
+		try{
+			loadLevel(level);
+		} catch(IOException e){
+			System.out.println("No file found." +e.getMessage());
 		}
+	}
+	// this will read the specified file
+	public static void loadLevel(String level) throws IOException{
+		try{
+			File file = new File(level);
+			String lvlPath = file.getAbsolutePath();
 
-		fr.close();
-		
-		int a= 0, b=0;
+			FileReader fr = new FileReader(lvlPath+".txt");
+			BufferedReader br = new BufferedReader(fr);
+			String line;
+			line = br.readLine();
+			String columnLength = line.substring(0,2);
+			String rowLength = line.substring(3,5);
+			
+			int column = Integer.parseInt(columnLength) + 1;
+			int row = Integer.parseInt(rowLength) + 1;
+			String [][]board = new String[column][row];
+
+			while((line= br.readLine()) != null){
+				String pos =line.substring(line.indexOf(" "));
+				String temp_i = pos.substring(pos.indexOf(" "), pos.lastIndexOf(" "));
+				String temp_j = pos.substring(pos.lastIndexOf(" "));
+				temp_i = temp_i.replaceAll("\\s","");
+				temp_j = temp_j.replaceAll("\\s","");
+				int i = Integer.parseInt(temp_i);  // col
+				int j = Integer.parseInt(temp_j); // row
+				board[i][j] = line.substring(0,line.indexOf(" "));
+				
+			}
+
+			fr.close();
+
+			int a= 0, b=0;
 		for(String sousTab[] : board){
 			a =0;
 			for(String str : sousTab)
@@ -41,6 +58,15 @@ public class Board{
 			b++;
 		
 		}
+		}
+		catch(IOException e){
+			System.out.println("No file found." +e.getMessage());
+		}
+	}
+
+	public String getElem(int i, int j){
+		Board board = new Board(level_1);
+		return board[i][j];
 	}
 	
 }
